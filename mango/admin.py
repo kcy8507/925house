a9b450f04bcc577586c5746d3d0fb71fcd70fb18
coffe2 <rkgus1274@gmail.com>
2025-04-18 14:40:04 +0900

    update admin-form

--- mango/admin.py
+++ mango/admin.py
@@ -1,34 +1,35 @@
-from django import forms
+# from django import forms
 from django.contrib import admin
 from mango.models import Request, Portfolio, Image
-from django.forms.models import BaseInlineFormSet
+# from django.forms.models import BaseInlineFormSet
 
 
-class ImageInlineFormSet(BaseInlineFormSet):
-    def save_new(self, form, commit=True):
-        files = form.files.getlist("image")
-        instances = []
-        for f in files:
-            instance = Image(portfolio=form.cleaned_data["portfolio"], image=f)
-            if commit:
-                instance.save()
-            instances.append(instance)
-        return instances
+# class ImageInlineFormSet(BaseInlineFormSet):
+#     def save_new(self, form, commit=True):
+#         files = form.files.getlist("image")
+#         instances = []
+#         for f in files:
+#             instance = Image(portfolio=form.cleaned_data["portfolio"], image=f)
+#             if commit:
+#                 instance.save()
+#             instances.append(instance)
+#         return instances
 
-class ImageInlineForm(forms.ModelForm):
-    image = forms.FileField(
-        widget=forms.FileInput(attrs={"multiple": True}),
-        required=False,
-    )
+# class ImageInlineForm(forms.ModelForm):
+#     image = forms.FileField(
+#         widget=forms.FileInput(attrs={"multiple": True}),
+#         required=False,
+#     )
 
-    class Meta:
-        model = Image
-        fields = ["image"]
+#     class Meta:
+#         model = Image
+#         fields = ["image"]
 
 class ImageInline(admin.StackedInline):
     model = Image
-    form = ImageInlineForm
-    formset = ImageInlineFormSet
+    # form = ImageInlineForm
+    # formset = ImageInlineFormSet
+    fields = ["image"]
     extra = 1
 
 class RequestAdmin(admin.ModelAdmin):
@@ -52,6 +53,7 @@ class RequestAdmin(admin.ModelAdmin):
 
 class PortfolioAdmin(admin.ModelAdmin):
     list_display = ["name", "created"]
+    readonly_fields = ["created"]
     inlines = [ImageInline]
 
 
